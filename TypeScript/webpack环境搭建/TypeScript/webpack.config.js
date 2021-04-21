de9236910210830f0bf5a1826814fd483cea4959
SanQiG <Sanqi_3737@163.com>
2021-04-22 01:41:40 +0800

    up ts

---
+++ TypeScript/webpack环境搭建/TypeScript/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    // 告诉 webpack 打包后的代码不使用箭头函数
+    environment: {
+      arrowFunction: false
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            // 配置 babel
+            options: {
+              // 设置预定义的环境
+              presets: [
+                [
+                  // 指定环境的插件
+                  "@babel/preset-env",
+                  // 配置信息
+                  {
+                    // 要兼容的目标浏览器
+                    targets: {
+                      chrome: 58,
+                      ie: 11
+                    },
+                    // 指定 corejs 的版本
+                    corejs: 3,
+                    // 使用 corejs 的方式，usage 表示按需加载
+                    useBuiltIns: "usage"
+                  }
+                ]
+              ]
+            }
+          },
+          'ts-loader'
+        ],
+        exclude: /node-modules/
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      // title: 'typescript-demo'
+      template: './src/index.html'
+    }),
+    new DashboardPlugin()
+  ],
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+};
