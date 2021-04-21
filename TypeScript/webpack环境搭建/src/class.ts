6b00469558ef8f48746add43b70eccab9efe6593
SanQiG <Sanqi_3737@163.com>
2021-04-22 03:37:12 +0800

    up ts

---
+++ TypeScript/webpack环境搭建/src/class.ts
@@ -0,0 +1,42 @@
+class Animal {
+  name: string;
+
+  constructor(name: string, age?: number) {
+    this.name = name;
+  }
+
+  say() {
+    console.log('hi');
+  }
+}
+
+class Dog extends Animal {
+  age: number;
+
+  constructor(name: string, age: number) {
+    super(name);
+    this.age = age;
+  }
+
+  say() {
+    super.say();
+    console.log(`${this.name} - ${this.age} 汪汪汪`);
+  }
+
+  run() {
+    console.log(`${this.name} - ${this.age} running`);
+  }
+}
+
+class Cat extends Animal {
+  say() {
+    console.log(`${this.name} 喵喵喵`);
+  }
+}
+
+const d1 = new Dog('?', 11);
+const d2 = new Cat('!');
+
+d1.say();
+d1.run();
+d2.say();
--- TypeScript/webpack环境搭建/src/index.ts
+++ TypeScript/webpack环境搭建/src/index.ts
@@ -1,19 +1,3 @@
 import { hi } from './m1';
 
-const sum = (a: number, b: number): number => {
-  return a + b;
-};
-
-const obj = { name: 'sanki', age: 0 };
-console.log('obj', obj);
-obj.age = 24;
-console.log('obj', obj);
-
-console.log('sum', sum(1, 3));
-console.log('hi', hi);
-
-Promise.resolve(1).then(res => console.log('res', res));
-
-let fn = (a: number, b: number): number => a + b;
-fn(123, 456);
-fn(77, 22);
+require('./class');
